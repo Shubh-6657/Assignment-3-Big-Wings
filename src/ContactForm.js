d7e982d011895190d42dacae6e36f7c953dfd506
Shubh Agrahari <108170423+Shubh-6657@users.noreply.github.com>
2023-09-10 17:31:01 +0530

    ContactForm.js

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,42 +1,58 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import Button from '@mui/material/Button'; 
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import './ContactForm.css';
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone: '',
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      phone: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required('Name is required'),
+      email: Yup.string().email('Invalid email address').required('Email is required'),
+      phone: Yup.string().matches(/^\d{10}$/, 'Invalid phone number').required('Phone Number is required'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission here (e.g., send data to an API)
-    console.log(formData);
-  };
-
   return (
-    <Paper elevation={3} style={{ padding: '20px', maxWidth: '400px', margin: '0 auto' }}>
-      <form onSubmit={handleSubmit}>
-      <h1>Please enter your Name,EmailID & Phone Number</h1>
+    <Paper elevation={3} className="contact-form">
+      <form onSubmit={formik.handleSubmit}>
+      <h1 style={{ fontSize: '16px' }}>Please enter your Name,EmailID & Phone Number</h1>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <TextField
               label="Name Surname"
               name="name"
-              value={formData.name}
-              onChange={handleChange}
               fullWidth
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              // style={textFieldStyle}
+              InputLabelProps={{
+                style: {
+                  fontSize: '14px', 
+                },
+              }}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  "& > fieldset": {
+                    border: 1,
+                    borderRadius: '50px',
+                  },
+                  "& input": {
+                    height: '20px', 
+                  },
+                },
+              }}
             />
           </Grid>
           <Grid item xs={12}>
@@ -44,9 +60,26 @@ const ContactForm = () => {
               label="Email"
               name="email"
               type="email"
-              value={formData.email}
-              onChange={handleChange}
               fullWidth
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              // style={textFieldStyle}
+              InputLabelProps={{
+                style: {
+                  fontSize: '14px', 
+                },
+              }}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  "& > fieldset": {
+                    border: 1,
+                    borderRadius: '50px', 
+                  },
+                  "& input": {
+                    height: '20px',
+                  },
+                },
+              }}
             />
           </Grid>
           <Grid item xs={12}>
@@ -54,17 +87,38 @@ const ContactForm = () => {
               label="Phone Number"
               name="phone"
               type="tel"
-              value={formData.phone}
-              onChange={handleChange}
               fullWidth
+              value={formik.values.phone}
+              onChange={formik.handleChange}
+              // style={textFieldStyle}
+              InputLabelProps={{
+                style: {
+                  fontSize: '14px', 
+                },
+              }}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  "& > fieldset": {
+                    border: 1,
+                    borderRadius: '50px',
+                  },
+                  "& input": {
+                    height: '20px',
+                  },
+                },
+              }}
             />
           </Grid>
           <Grid item xs={12}>
-            <Button variant="contained" color="primary" type="submit" fullWidth>
-              Submit
+            <Button variant="contained" color="primary" type="submit"  fullWidth
+              style={{
+                borderRadius: '20px', // You can adjust the value to control the rounding
+              }}>
+              Next
             </Button>
           </Grid>
         </Grid>
+        <h5 style={{ textAlign: 'center' }}>Already a member? Click on <a href="#">LOG IN.</a></h5>
       </form>
     </Paper>
   );
