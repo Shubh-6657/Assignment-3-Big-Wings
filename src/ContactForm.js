63a8d20c814b8dd354d6eb08056c5005cf918b87
Shubh Agrahari <108170423+Shubh-6657@users.noreply.github.com>
2023-09-18 10:16:15 +0530

    ContactForm.js

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -5,8 +5,14 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'; 
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import FacebookIcon from '@mui/icons-material/Facebook'; 
 import './ContactForm.css';
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; 
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone'; 
+import FacebookIcon from '@mui/icons-material/Facebook'; 
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter'; 
 
 const ContactForm = () => {
   const formik = useFormik({
@@ -50,91 +56,120 @@ const ContactForm = () => {
         </h1>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-          <TextField
-          label="Name Surname"
-          name="name"
-          fullWidth
-          value={formik.values.name}
-          onChange={formik.handleChange}
-          InputLabelProps={{
-            style: { fontSize: '14px', ...blackLabelStyle }, 
-          }}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              borderRadius: '30px', 
-              border: '1px solid white', 
-            },
-            "& .MuiOutlinedInput-input": {
-              height: '16px',
-              ...whiteTextStyle,
-              background: 'white', 
-              borderRadius: '30px',            
-            },
-          }}
-        />
-          </Grid>
+          <Grid item xs={12}>
           <Grid item xs={12}>
           <TextField
-            label="Email"
-            name="email"
+            label="Name Surname"
+            name="name"
             fullWidth
             value={formik.values.name}
             onChange={formik.handleChange}
             InputLabelProps={{
-              style: { fontSize: '14px', ...blackLabelStyle }, 
+              style: { fontSize: '14px', ...blackLabelStyle },
+            }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <AccountCircleIcon style={{ color: '#00aeef' }} /> 
+                </InputAdornment>
+              ),
             }}
             sx={{
               "& .MuiOutlinedInput-root": {
                 borderRadius: '30px', 
-                border: '1px solid white', 
+                border: '1px solid white',
+                background: 'white', 
               },
               "& .MuiOutlinedInput-input": {
                 height: '16px',
                 ...whiteTextStyle,
-                background: 'white', 
-                borderRadius: '30px',
+                borderRadius: '30px', 
               },
             }}
           />
-          </Grid>
+</Grid>
+</Grid>
+        </Grid>
+        <Grid item xs={12}>
+        <TextField
+          label="Email"
+          name="email"
+          fullWidth
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          InputLabelProps={{
+            style: { fontSize: '14px', ...blackLabelStyle },
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <EmailIcon style={{ color: '#00aeef' }} /> 
+              </InputAdornment>
+            ),
+          }}
+          sx={{
+            "& .MuiOutlinedInput-root": {
+              borderRadius: '30px', 
+              border: '1px solid white',
+              background: 'white', 
+            },
+            "& .MuiOutlinedInput-input": {
+              height: '16px',
+              ...whiteTextStyle,
+              borderRadius: '30px', 
+            },
+          }}
+        />
+</Grid>
           <Grid item xs={12}>
           <TextField
             label="Phone Number"
             name="phone number"
             fullWidth
-            value={formik.values.name}
+            value={formik.values.phoneNumber} 
             onChange={formik.handleChange}
             InputLabelProps={{
-              style: { fontSize: '14px', ...blackLabelStyle }, 
+              style: { fontSize: '14px', ...blackLabelStyle },
+            }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <PhoneIcon  style={{ color: '#00aeef' }} />
+                </InputAdornment>
+              ),
             }}
             sx={{
               "& .MuiOutlinedInput-root": {
-                borderRadius: '30px', 
-                border: '1px solid white', 
+                borderRadius: '30px',
+                border: '1px solid white',
+                background: 'white', 
               },
               "& .MuiOutlinedInput-input": {
                 height: '16px',
                 ...whiteTextStyle,
-                background: 'white', 
                 borderRadius: '30px', 
               },
             }}
           />
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained" color="primary" type="submit"  fullWidth
-              style={{
-                borderRadius: '20px',
-              }}>
-              Next
-            </Button>
-          </Grid>
-        </Grid>
-        <h5 style={{ textAlign: 'center', ...whiteTextStyle }}>
-          Already a member? Click on <a href="#">LOG IN.</a>
-        </h5>
-      </form>
-    </Paper>
+</Grid>
+<Grid item xs={12}>
+  <Button variant="contained" color="primary" type="submit" fullWidth style={{borderRadius: '20px',
+      backgroundColor: '#00aeef',color: 'white', }}>
+          Next
+  </Button>
+</Grid>
+</Grid>
+<h5 style={{ textAlign: 'center', ...whiteTextStyle }}>
+  Already a member? Click on <a href="#" style={{ color: '#00aeef' }} >LOG IN.</a>
+  <br /> <br/>
+  <FacebookIcon style={{ color: '#FFFFFF', fontSize: '24px', marginRight: '8px' }} />
+  <InstagramIcon style={{ color: '#FFFFFF', fontSize: '24px', marginRight: '8px' }} />
+  <TwitterIcon style={{ color: '#FFFFFF', fontSize: '24px' }} />
+</h5>
+
+
+  </form>
+ </Paper>
   );
 };
 
